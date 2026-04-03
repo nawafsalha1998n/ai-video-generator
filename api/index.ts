4b2eb4f2c51f49c62a48197954b18b0f013e97e7
nawafsalha1998n <170742766+nawafsalha1998n@users.noreply.github.com>
2026-04-03 15:01:10 -0400

    Fix module resolution by using .ts extensions for ESM compatibility on Vercel (Final Fix 6)

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,9 @@
 import "dotenv/config";
 import express from "express";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { registerOAuthRoutes } from "../server/_core/oauth.js";
-import { appRouter } from "../server/routers.js";
-import { createContext } from "../server/_core/context.js";
+import { registerOAuthRoutes } from "../server/_core/oauth.ts";
+import { appRouter } from "../server/routers.ts";
+import { createContext } from "../server/_core/context.ts";
 
 const app = express();
 
